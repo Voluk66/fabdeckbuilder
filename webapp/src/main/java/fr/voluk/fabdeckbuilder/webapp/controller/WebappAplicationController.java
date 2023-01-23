d359bd1edd5de6f592a0eafcd4b9f418d3f09098
Voluk66 <maitrot@et.esiea.fr>
2023-01-23 04:08:18 +0100

    Init

---
+++ webapp/src/main/java/fr/voluk/fabdeckbuilder/webapp/controller/WebappAplicationController.java
@@ -0,0 +1,16 @@
+package fr.voluk.fabdeckbuilder.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WebappAplicationController {
+    @RequestMapping(value = {"/","/home"}, method = RequestMethod.GET)
+    public ModelAndView home(){
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+
+}
---
+++ webapp/src/test/java/fr/voluk/fabdeckbuilder/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.voluk.fabdeckbuilder.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
